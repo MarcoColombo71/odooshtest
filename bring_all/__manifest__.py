6a32579a23817f0d9c9bf10f4d543abbdb851a66
Marco Colombo <marco.colombo@gmail.com>
2022-10-26 18:54:51 +0200

    [ADD] bring_all

---
+++ bring_all/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "ITA - Bring all",
+    "version": "14.0.1.0.0",
+    "category": "Account",
+    "author": "Openforce, Odoo Italia Network, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_it_fatturapa_in",
+    ],
+    "installable": True,
+    "development_status": "Beta",
+}
